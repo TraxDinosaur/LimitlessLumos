378d27d3ca615e341d5111331998d3070b8e0731
TraxDinosaur <imtraxdinosaur@gmail.com>
2024-08-15 12:42:21 +0530

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 # Package metadata
 NAME = "LimitlessLumos"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 DESCRIPTION = "A package providing a Flask-based server to keep Telegram bots and other scripts running indefinitely."
 URL = "https://github.com/TraxDinosaur/LimitlessLumos"
 AUTHOR = "TraxDinosaur"
